cfed5bbc08c41aad938e67045e3062bd69f35380
Omar Ihmoda <omarihmoda@Omars-MacBook-Pro.local>
2018-01-12 15:37:22 -0800

    made into another project

--- Aging People/ViewController.swift
+++ Aging People/ViewController.swift
@@ -11,58 +11,69 @@ import UIKit
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate  {
     
     var names = [
-        "George",
-        "Betty",
-        "Fran",
-        "Joe",
-        "Helda",
-        "Winifred",
-        "Zed",
-        "Sara",
-        "Jeffy",
-        "Abraham",
-        "Anna",
-        "Melinda"
-        ]
-    var tasks: [(String, String)] = []
-
+        "Red",
+        "Yellow",
+        "Orange",
+        "Blue",
+        "Green",
+        "Purple"
+    ]
+    
+    
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
         tableView.delegate = self
-        self.getPeople()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    func getPeople(){
-        var random_num: Int = 0
-        var age_string: String = ""
-        for name in self.names {
-            random_num = Int(arc4random_uniform((90)) + 6)
-            age_string = "\(random_num) years old"
-            self.tasks.append((name, age_string))
-        }
-        print(self.tasks)
-    }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.tasks.count
+        return self.names.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
-        cell.textLabel?.text = self.tasks[indexPath.row].0
-        cell.detailTextLabel?.text = self.tasks[indexPath.row].1
+        
+        let color = self.names[indexPath.row]
+        
+        switch color {
+            case "Green":
+                cell.backgroundColor = .green
+            case "Red":
+                cell.backgroundColor = .red
+            case "Yellow":
+                cell.backgroundColor = .yellow
+            case "Orange":
+                cell.backgroundColor = .orange
+            case "Blue":
+                cell.backgroundColor = .blue
+            case "Purple":
+                cell.backgroundColor = .purple
+            default:
+                print("no color")
+        }
+        
+        
+        cell.textLabel?.text = ""
+        cell.detailTextLabel?.text = ""
+
         return cell
     }
-
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        let tHeight = tableView.bounds.height
+        let temp = tHeight/CGFloat(names.count)
+        return temp
+    
+    }
 
 }
 
